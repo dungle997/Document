90799d4da3bec776537e31cd9907c3c4db3f33db
dunglq <dunglq@bkav.com>
2022-03-17 17:09:04 +0700

    DungLQ cap nhat Java Script

---
+++ JavaScript/Bien/main.js
@@ -0,0 +1,70 @@
+// var fullName = 'Le qung dung';
+// alert(fullName);
+// alert('asdfasf');
+// console.log(fullName);
+// console.error(fullName);
+
+// confirm('Xac nhan ban dung');
+// prompt('Xac nhan ban du tuoi');
+// setTimeout(function(){
+//     alert('Thong Bao')
+// }, 1000)
+// setInterval(function(){
+//     console.log('alo alo alo ' + Math.random())
+// }, 2000)
+// var a = 7, b =2.12563;
+// var c = 1;
+// c += a;
+// console.log(c);
+
+// --------------------------------------------------------
+// var firstName = 'Le';
+// var lastName = 'Dung';
+// firstName += ' firstName';
+
+// console.log(firstName);
+// --------------------------------------------------------
+// var a =5 ;
+// var b =2 ;
+// var isSuccess = a >b;
+// console.log(isSuccess);
+// -------------------------------------------------------
+/* 
+if else
+- 0 
+- false
+- '' - ""
+- undefined
+- NaN
+- null
+=> convert sang boolean là false
+còn lại là đều đúng 
+*/
+
+/*
+    Kiểu dữ liệu trong JavaScript
+
+    1. Dữ liệu nguyên thủy - Primitive Data
+        - Number
+        - String
+        - Boolean
+        - Undefined
+        - Null
+        - Symbol
+
+    2. Dữ liệu phức tạp - Complex Data
+        - Function
+        - Object
+*/
+// undefined
+var undefined;
+console.log(undefined);
+// null
+var isNull = null; // nothing
+console.log(isNull);
+
+// Symbol
+
+// Toán tử so sánh === không thực hiện chuyển đổi loại 
+// Toán tử so sánh == có thực hiện chuyển đổi loại. VD: 9 == "9" +> true | 9 === "9" => false 
+
