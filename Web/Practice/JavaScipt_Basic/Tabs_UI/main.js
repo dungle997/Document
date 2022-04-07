ba42e18a76d866b6626aa562507ef6edebeca1c6
dung <quangdung31297@gmail.com>
2022-04-07 23:00:28 +0700

    Cap nhat tabUI JS practice

--- Web/Practice/JavaScipt_Basic/Tabs_UI/main.js
+++ Web/Practice/JavaScipt_Basic/Tabs_UI/main.js
@@ -1,13 +1,37 @@
-const tabItems = document.querySelectorAll('.tab-item')
-// console.log(tabItems)
+var $ = document.querySelector.bind(document)
+var $$ = document.querySelectorAll.bind(document)
 
-// tabItems.forEach(function(tabItem){
-    
-// })  
-tabItems.forEach((tabItem) => {
-    console.log(tabItem)
+const tabItem = $('.tab-item')
+const tabItems = $$('.tab-item')
+const tabPane = $('.tab-pane')
+const tabPanes = $$('.tab-pane')
+
+const tabActive = $('.tab-item.active')
+const line = $('.tabs .line') // thêm dấu cách vì không cùng cấp
+
+// console.log([tabActive])
+// console.log([line])
+
+line.style.left = tabActive.offsetLeft + 'px'
+line.style.width = tabActive.offsetWidth + 'px'
+
+
+
+
+tabItems.forEach((tabItem, index) => {
     tabItem.onclick = function(){
-     console.log(this)
-     tabItem.className = 'tab-item active'
+    $('.tab-item.active').classList.remove('active')
+    console.log([this])
+    this.classList.add('active')
+    $('.tab-pane.active').classList.remove('active')
+    tabPanes[index].classList.add('active') 
+
+    line.style.left = this.offsetLeft + 'px'
+    line.style.width = this.offsetWidth + 'px'
+
     }
 })
+
+    // $('.tab-pane.active').classList.remove('active')
+
+    
