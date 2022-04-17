1d8dd4a75f904947dc965eb674df4a92abc0d82c
dunglq <dunglq@bkav.com>
2022-04-17 18:46:49 +0700

    Cap nhat app Music

--- Web/Practice/JavaScipt_Basic/Code_Music_Player_test/assets/js/main.js
+++ Web/Practice/JavaScipt_Basic/Code_Music_Player_test/assets/js/main.js
@@ -16,10 +16,25 @@ const $$ = document.querySelectorAll.bind(document);
 
 const playlist = $('.playlist');
 const image = $('.image');
+const headerH2 = $('header h2');
+const imageSong = $('.image-cd');
+const audio = $('#audio');
+const player = $('.player');
+const playBtn = $('.btn-play');
+const pauseBtn = $('.btn-pause');
+const progress = $('#progress');
+const forwardBtn = $('.btn-forward');
+const backwardBtn = $('.btn-backward');
+const randomBtn = $('.btn-random');
+const redoBtn = $('.btn-redo');
+
+
 
 
 const app = {
     currentIndex : 0,
+    isRandom : false,
+    isRepeat : false,
     songs : [
         {
             name: 'Song 1',
@@ -83,21 +98,25 @@ const app = {
         },
     ],
     render: function(){
-        const htmls = this.songs.map(function(song,index){
+        // const htmls = this.songs.map(function(song,index) {
+        // console.log(this) // Viết như này thì không lấy được con trỏ this nên ko thêm đc class active
+        const htmls = this.songs.map((song,index) => { // Viết như này thì mới lấy được con trỏ this  
             return `
-                <div class="song">
+                <div class="song ${
+                    index === this.currentIndex ? "active" : ""
+                  }">
                     <div class="song-image"></div>
                     <div class="song-body">
                         <h3 class="title">${song.name}</h3>
                         <p class="author">${song.singer}</p>
                     </div>
                     <div class="song-option">
-                    <i class="fa-solid fa-ellipsis+"></i>
+                    <i class="fa-solid fa-ellipsis"></i>
                     </div>
                 </div>
                     `
         })
-        playlist.innerHTML = htmls.join('');      
+        playlist.innerHTML = htmls.join('');    
         // show background
         const pathImage = playlist.querySelectorAll('.song-image');
         this.songs.forEach(function(song, index){ 
@@ -105,6 +124,17 @@ const app = {
         })
     },
     handleEvents: function(){
+        const _this = this;
+        // Xử lí quay ảnh image-cd
+        const spinImage = imageSong.animate([{
+        transform: 'rotate(360deg)'
+        }],{
+            duration: 10000,
+            iterations: Infinity,  
+        })
+        spinImage.pause()
+
+        // Xử lí khi phóng to thu nhỏ CD 
         const imageWidth = image.offsetWidth;
         document.onscroll = function(){
             // console.log(window.scrollY)
@@ -120,7 +150,152 @@ const app = {
             image.style.width = newWidth > 0 ? newWidth + 'px' : 0;
             image.style.opacity = newWidth / imageWidth;
         }
+        // Xử lí khi click play cách 1
+        // playBtn.onclick = function(){
+        //     audio.play();
+        //     player.classList.add('playing');
+
+        // }
+        // pauseBtn.onclick = function(){
+        //     audio.pause();
+        //     player.classList.remove('playing');
+        // }
+        // xử lí khi click play cách 2 Tạo cờ để phân biệt 2 trạng thái
+        // Để cả 2 thẻ i của nút pause và play trong cùng 1 khối 
+        // Tạo thêm một biến trạng thái isPlaying.
+        // Tạo một con trỏ _this = this trong hàm này để sử dụng đối tượng app
+        // playBtn.onclick = function(){
+        //     if (_this.isPlaying){
+        //         audio.pause()
+        //     }else{
+        //         audio.play()
+        //     }
+        // }
+
+        // audio.onplay = function(){
+        //     _this.isPlaying = true
+        //     player.classList.add('playing')
+        // }
+        // audio.onpause = function(){
+        //     _this.isPlaying = false
+        //     player.classList.remove('playing')
+        // }
+        // xử lí khi click play cách 3
+        playBtn.onclick = function(){
+            audio.play();
+        }
+        pauseBtn.onclick = function(){
+            audio.pause();
+        }
+        audio.onplay = function(){
+            audio.play()
+            player.classList.add('playing')
+            spinImage.play()
+        }
+        audio.onpause = function(){
+            audio.pause()
+            player.classList.remove('playing')
+            spinImage.pause()
+        }
+        // Xử lí khi tiến độ bài hát thay đổi. Thay đổi input progress.value
+        
+        audio.ontimeupdate = function(){
+            if (audio.duration){
+                const progressPercent = audio.currentTime / audio.duration * 100
+                // console.log(progressPercent)
+                progress.value = progressPercent
+            }
+        }
+
+        // Xử lí khi tua song
+        progress.onchange = function(e){
+            const seekTime = audio.duration * e.target.value / 100
+            audio.currentTime = seekTime 
+        }
+        // Xử lí khi click vào nút ramdom cho nút ramdom sáng lên
+            randomBtn.onclick = function(e){
+            _this.isRandom = !_this.isRandom
+            randomBtn.classList.toggle('active', _this.isRandom)
+            
+        }
+          // Xử lí bật tắt hiển thị khi click repeat bài hát
+          redoBtn.onclick = function(){
+            _this.isRepeat = !_this.isRepeat
+            redoBtn.classList.toggle('active', _this.isRepeat)
+        }
+        // Xử lí khi bấm next 
+        forwardBtn.onclick = function(){
+            if (_this.isRandom){
+                _this.randomSong();
+            }else {
+                _this.nextSong();
+            }
+            audio.play();
+            _this.render(); // có thể làm cách remove class active đi rồi thêm lại cho song sau
+            // Chọn tất cả thẻ song, tìm song có chứa class .active rồi xóa đi
+            // Chọn song có index bằng currentIndex rồi thêm class active vào
+        }
+        // Xử lí khi bấm pre
+        backwardBtn.onclick = function(){
+            if (_this.isRandom){
+                _this.randomSong();
+            }else {
+                _this.preSong();
+            }
+            audio.play();
+            _this.render();
+        }
+
+        // Xử lí khi kết thúc bài hát và repeat bài hát 
+        audio.onended = function(){
+            // if(_this.isRepeat){
+            //     _this.repeatSong();
+            // }else{
+            //     forwardBtn.onclick()
+            // }
+            if(_this.isRepeat){
+                audio.play()
+            }else{
+                forwardBtn.onclick()
+            }
+        }
+
+      
+
+      
+
+
+
+
     },
+    nextSong: function(){
+        this.currentIndex++
+        if (this.currentIndex >= this.songs.length){
+            this.currentIndex = 0;
+        }
+        this.loadCurentSong();
+    }, 
+    preSong: function(){
+        this.currentIndex--
+        if(this.currentIndex < 0){
+            this.currentIndex = this.songs.length - 1;
+        }
+        this.loadCurentSong();
+    },
+    randomSong: function(){
+        let newIndex
+        do{
+            newIndex = Math.floor(Math.random() * this.songs.length)
+        }
+        while (newIndex === this.currentIndex)
+        this.currentIndex = newIndex;
+        this.loadCurentSong();
+    },
+    // repeatSong: function(){
+    //     this.loadCurentSong();
+    //     audio.play()
+    // },
+
     defineProperty: function(){
         Object.defineProperty(this, 'currentSong', {
             get : function(){
@@ -128,8 +303,14 @@ const app = {
             }
         })
     },
+    loadCurentSong: function(){
+        headerH2.textContent = this.currentSong.name;
+        imageSong.style.backgroundImage = `url(${this.currentSong.image})`
+        audio.src = this.currentSong.path
+    },
     start: function(){
         this.defineProperty();
+        this.loadCurentSong();
         this.handleEvents();
 
         this.render();
