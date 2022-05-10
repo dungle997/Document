a92a943881b6f4880c1684816fd8a6d2ba684fa6
dunglq <dunglq@bkav.com>
2022-05-10 18:21:27 +0700

    Cap nhat bind call method

--- Web/JavaScript_Advance/6.BindMethod/main.js
+++ Web/JavaScript_Advance/6.BindMethod/main.js
@@ -80,36 +80,158 @@
 
 
 
-// // Lưu ý: 
-// // - Phương thức bind() sẽ trả về một hàm mới với 1 cái this mới . Là vùng nhớ mới đc tạo ra
-// // - Có thể nhân các đối số như hàm ban đầu
 
 
 
 // VD 2
 
-this.firstName = "Minh"
-this.lastName = "Thu"
+// this.firstName = "Minh"
+// this.lastName = "Thu"
 
-const teacher = {
-    firstName: "Minh",
-    lastName: "Thảo",
-    getFullName(data1, data2){
-        console.log(data1, data2)
-        return `${this.firstName} ${this.lastName}`
+// const teacher = {
+//     firstName: "Minh",
+//     lastName: "Thảo",
+//     getFullName(data1, data2){
+//         console.log(data1, data2)
+//         return `${this.firstName} ${this.lastName}`
+//     }
+// }
+
+// const student = {
+//     firstName: "Dũng",
+//     lastName: "Lê",
+// }
+// // kịch bản ban đầu
+// // console.log(teacher.getFullName()) // Minh Thảo
+
+// // const getTeacherName = teacher.getFullName.bind(student)
+// const getTeacherName = teacher.getFullName.bind(student, 'Test3', 'Test4') // các đối số test3, test 4 sẽ được ưu tiên, lúc này test 1 , test 2 không được thực thi
+// // khi cần truyền đối số cố định thì truyền thẳng vào bind (bind đối số)
+
+// console.log(getTeacherName('Test1', 'Test2')) // cần truyền đối số động thì truyền như này và bỏ phần truyền ở bind đi
+
+//VD3: Ví dụ về DOM
+
+// const teacher = {
+//     firstName: "Minh",
+//     lastName: "Thảo",
+//     getFullName(){
+//         console.log(`${this.firstName} ${this.lastName}`)
+//     }
+// }
+
+// const button = document.querySelector('button')
+// // Case 1
+// // button.onclick = function(){
+//     // teacher.getFullName() //gọi qua đối tượng thì luôn chạy đúng
+// // }   
+// // Case 2
+
+// button.onclick = teacher.getFullName.bind(teacher)
+
+// VD4: 
+// const $ = document.querySelector
+// const $$ = document.querySelectorAll
+// console.log($('#heading')) // Lỗi 
+// // Do trong querySelector có sử dụng từ khoá this. Khi sử dụng $ thì gọi phương thức querySelector không thông qua đối tượng nên
+// // con trỏ this trỏ về global (window)
+
+
+// const $ = document.querySelector.bind(document) // ràng buộc từ khóa this trong hàm querySelector với đối tượng document
+// const $$ = document.querySelectorAll.bind(document)
+// // console.log($)
+// console.log($('#heading').innerText)
+// console.log($('#heading'))
+
+// VD 5
+
+const $ = document.querySelector.bind(document) // ràng buộc từ khóa this trong hàm querySelector với đối tượng document
+const $$ = document.querySelectorAll.bind(document)
+// IIFE
+const app = (() => {
+    const cars = ['BMW']
+    const input = $('#input')
+    const add = $('#add')
+    // console.log(add)
+    const root = $('#root')
+    // console.log('log1',this)
+
+    return{
+        add(car){
+            cars.push(car)
+        },
+        delete(index){
+            cars.splice(index, 1)
+        },
+        render(){
+            // sử dụng data-set js để xóa 
+            const html = cars.map(function(car, index){
+                return `
+                        <li>
+                            ${car}
+                            <span class="delete" data-index="${index}">&times</span>
+                        </li>
+                        `
+            }).join('')
+            root.innerHTML = html
+        },
+        // Các element được thêm vào sau khi add, vì vậy nếu để lắng nghe sự kiện trước khi element đc thêm vào dom thì sẽ ko đc
+        // Thông thường viết phần lắng nghe sau phần render. Nếu lắng nghe kiểu cứng là load hết element lên để lắng nghe thì sẽ gặp tình trạng trên
+        // nên viết theo kiểu delegate pattern có thể bắt được click thêm vào dom sau khi chương trinhf chạy
+        // Lắng nghe ở thẻ cha, thẻ root. Từ thẻ cha đó có thẻ xác định được đối tượng nào được tác động vào, vì luôn nghe ở thẻ cha nên sẽ đầy đủ nhất
+        init(){
+            // console.log('log2',this)
+            add.onclick = () => {
+                console.log(this)
+                console.log(input.value)
+                if (input.value){
+                    this.add(input.value)
+                    this.render()
+                    input.value = null
+                    input.focus()
+                }
+            }
+            input.addEventListener("keypress", function(event) {
+                // If the user presses the "Enter" key on the keyboard
+                if (event.key === "Enter") {
+                  // Cancel the default action, if needed
+                  event.preventDefault();
+                  // Trigger the button element with a click
+                  document.getElementById("add").click();
+                }
+            })
+            this.render()
+            root.onclick = this.handleDelete.bind(this)
+
+        }, 
+        handleDelete(e){
+            // console.log(e.target)
+            if (e.target.closest('.delete')){
+                const index = e.target.dataset.index
+                this.delete(index)
+                this.render()
+            }
+        }
     }
-}
 
-const student = {
-    firstName: "Dũng",
-    lastName: "Lê",
-}
-// kịch bản ban đầu
-// console.log(teacher.getFullName()) // Minh Thảo
+})()
+// console.log(app)
+app.init()
 
-// const getTeacherName = teacher.getFullName.bind(student)
-const getTeacherName = teacher.getFullName.bind(student, 'Test3', 'Test4') // các đối số test3, test 4 sẽ được ưu tiên, lúc này test 1 , test 2 không được thực thi
-// khi cần truyền đối số cố định thì truyền thẳng vào bind (bind đối số)
 
-console.log(getTeacherName('Test1', 'Test2')) // cần truyền đối số động thì truyền như này và bỏ phần truyền ở bind đi
 
+
+
+// // Lưu ý: 
+// // - Phương thức bind() sẽ trả về một hàm mới với 1 cái this mới . Là vùng nhớ mới đc tạo ra
+// // - Có thể nhân các đối số như hàm ban đầu
+
+// Nội dung ví dụ:
+// 1. Ví dụ bind document object
+// 2. Ví dụ DOM listen events
+// 3. Ví dụ kết hợp giữa object methods + DOM listen events
+
+// Tóm tắt: 
+// - Phương thức bind() cho phép ràng buộc this cho phương thức (function)
+// - Phương thức bind() sẽ trả về một hàm mới với context được bind
+// - Hàm được trả về từ bind() vẫn có thể nhận các đối số của hàm gốc
--- Web/JavaScript_Advance/7.Callmethod/main.js
+++ Web/JavaScript_Advance/7.Callmethod/main.js
@@ -1,67 +1,107 @@
-// --------------------------------------------------------- Scope ---------------------------------------------------------------
-// function show(){
-//     var a = 5 
-//     if (a > 0) {
-//         var b = 6
-//         var b = 8
-//     }
-//     console.log(b)
+// Call Method : bind + call
+
+// Là phương thức trong prototype của Function constructor, phương thức này 
+// được dùng để gọi hàm và cũng có thể bind this cho hàm
+// Ví dụ
+// - Gọi hàm với call method
+// - Gọi hàm và bind this, lưu ý trong strict mode vẫn có this nếu được bind
+// - Thể hiện tính kế thừa (extends) trong OOP 
+// - Mượn hàm (function borrowing), thêm ví dụ với arguments
+
+
+// ========================== - Gọi hàm với call method ============================================
+// VD 1:
+// function random(){
+//     console.log(Math.random())
 // }
 
-// show()
+// // random()
+// // Khi gọi hàm như vậy thì engine của JS sẽ gọi đến phương thức call để thực hiện gọi hàm
 
-// var a= 18 // Biến global. Tương tự với let
+// random.call()
 
-// {   
-//     var a= 16
-//     {
-//         var a= 14
-
-//         {
-//             var a= 12
-            
-//             {
-//                 console.log(a) //12
-//                 var a= 10
-//             }
-//         }
-//     }
+// VD2: Kĩ thuật mượn hàm
+// const teacher = {
+//     firstName: "Minh",
+//     lastName: "Thu",
 // }
 
-// let a= 18 // Biến global. Tương tự với let
-
-// {   
-//     let a= 16
-//     {
-//         let a= 14
-
-//         {
-//             let a= 12
-            
-//             {
-//                 console.log(a) //12
-//                 let a= 10
-//             }
-//         }
+// const me = {
+//     firstName: "Lê",
+//     lastName: "Dũng",
+//     showFullName(){
+//         console.log(`${this.firstName} ${this.lastName}`)
 //     }
 // }
 
-//   Biến trong hàm được tham chiếu bởi 1 hàm?
+// me.showFullName.call(teacher)
 
-// function makeCounter(){
-//     let counter = 0
-//     function increase(){
-//         return ++counter
-//     }
-//     return increase
+//=================================== - Gọi hàm và bind this, lưu ý trong strict mode vẫn có this nếu được bind ========================================
+// this.firstName = "Sơn"
+// this.lastName = "Đặng"
+
+// function showFullName(){
+//     console.log(`${this.firstName} ${this.lastName}`)
 // }
 
-// const increase1 = makeCounter()
+// showFullName() // Sơn Đặng
+// // do this lúc này trỏ đến window 
 
-// console.log(increase1())
-// console.log(increase1())
-// console.log(increase1())
-// console.log(increase1())
-// console.log(increase1())
 
-// ---------------------------------------------------------------------------- Closure ----------------------------------------------------------
+// -------
+// this trong use strict trỏ về undefined. Nhưng vẫn có nếu được bind
+// 'use strict'
+// this.firstName = "Sơn"
+// this.lastName = "Đặng"
+
+// function showFullName(){
+//     console.log(`${this.firstName} ${this.lastName}`)
+// }
+
+// // sử dụng bind 
+// // const fullName = showFullName.bind(this)
+// // fullName()
+// // sử dụng call
+
+// showFullName.call(this)
+
+// ===============================================- Thể hiện tính kế thừa (extends) trong OOP ===========================================
+// function Animal(name, weight){
+//     this.name = name
+//     this.weight = weight
+// }
+
+// function Chicken(name, weight, legs){
+//     Animal.call(this, name, weight)
+//     this.legs = legs
+// }
+
+// const dungLQ = new Chicken('Dũng Lê', 68, 2)
+// console.log(dungLQ)
+
+//================================================================= - Mượn hàm (function borrowing), thêm ví dụ với arguments=================================================================
+// logger
+// function logger(){
+//     Array.prototype.forEach.call(arguments, item =>{
+//         console.log(item)
+//     })
+
+//     // const cars = ['BMW', 'Honda']
+//     // cars.forEach(car =>{
+//         // console.log(car)
+//     // })
+// }
+
+// logger(1,2,3,4,5)
+
+// ====================================
+function logger(){
+//    const arr = Array.from(arguments)
+//    console.log(arr)
+
+   //ES6
+   const arr = [...arguments]
+   console.log(arr)
+}
+
+logger(1,2,3,4,5)
