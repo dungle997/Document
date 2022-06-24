9cec3766fadff5f6ef7eaac80dabf0fa5078cee9
dunglq <dunglq@bkav.com>
2022-06-24 17:16:28 +0700

    Cap nhat redux part 2

--- Web/ReactJS/9.Redux/todo-app-redux-main/src/components/TodoList/index.js
+++ Web/ReactJS/9.Redux/todo-app-redux-main/src/components/TodoList/index.js
@@ -1,26 +1,60 @@
 import { Col, Row, Input, Button, Select, Tag } from 'antd';
 import Todo from '../Todo';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addTodo } from '../../redux/actions'
+import { v4 as uuidv4} from 'uuid'
+import { useState } from 'react'
+import { todoListSelector } from '../../redux/selectors'
 
 export default function TodoList() {
+  const [todoName, setTodoName] = useState('')
+  const [priority, setPriority] = useState('Medium')
+  // console.log(todoName)
+
+  const todoList = useSelector(todoListSelector)
+  // console.log(todoList)
+
   const dispatch = useDispatch()
+
   const handleAddButtonClick = () => {
-    dispatch(addTodo({
-      
+    dispatch(
+      addTodo({
+        id: uuidv4(),
+        name: todoName, 
+        priority: priority,
+        completed: false
     }))
+
+    setTodoName('')
+    setPriority('Medium')
   }
+
+  const handleInputChange = (e) => {
+    // console.log(e.target.value)
+    setTodoName(e.target.value)
+  }
+
+  const handlePriorityChange = (value) => {
+    // console.log({a})
+    setPriority(value)
+  }
+
   return (
     <Row style={{ height: 'calc(100% - 40px)' }}>
       <Col span={24} style={{ height: 'calc(100% - 40px)', overflowY: 'auto' }}>
-        <Todo name='Learn React' prioriry='High' />
+        {/* <Todo name='Learn React' prioriry='High' />
         <Todo name='Learn Redux' prioriry='Medium' />
-        <Todo name='Learn JavaScript' prioriry='Low' />
+        <Todo name='Learn JavaScript' prioriry='Low' /> */}
+        {
+          todoList.map( todo => <Todo key={todo.id} name={todo.name} prioriry={todo.priority} />)
+        }
       </Col>
       <Col span={24}>
         <Input.Group style={{ display: 'flex' }} compact>
-          <Input />
-          <Select defaultValue="Medium">
+          {/* <Input value={todoName} onChange={e => handleInputChange(e)}/> */}
+          <Input value={todoName} onChange={handleInputChange}/>
+
+          <Select defaultValue="Medium"  value={priority} onChange={handlePriorityChange}>
             <Select.Option value='High' label='High'>
               <Tag color='red'>High</Tag>
             </Select.Option>
--- Web/ReactJS/9.Redux/todo-app-redux-main/src/redux/reducer.js
+++ Web/ReactJS/9.Redux/todo-app-redux-main/src/redux/reducer.js
@@ -20,15 +20,15 @@ const rootReducer = (state = initState, action) =>{
         }
     */
 
+    // console.log(state, action)
+
     switch(action.type){
         case 'todoList/addTodo':
             return {
                 ...state, 
                 todoList: [
                     ...state.todoList, 
-                    // action.payload
-                    {id: 3, name: 'Learn JavaScript', completed: false, priority: 'Low'}
-
+                    action.payload
                 ]
             }
         default: 
