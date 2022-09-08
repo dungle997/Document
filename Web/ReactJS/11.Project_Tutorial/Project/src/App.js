f98ca64103ccff1a9f6771b9652120fcd1f9046c
dunglq <dunglq@bkav.com>
2022-09-08 18:36:50 +0700

    Cap nhat tutorial project ReactJS

---
+++ Web/ReactJS/11.Project_Tutorial/Project/src/App.js
@@ -0,0 +1,14 @@
+import {Button} from 'antd'
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <Button type="primary">Button</Button>
+            {/* <Header></Header> */}
+            <p>     Con me chung no </p>
+        </div>
+    )
+}
+export default App;
