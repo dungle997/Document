759d45b7b671960ab612bc237473751c4d59bcaa
phamnhung <phamnhungmta@gmail.com>
2022-09-27 06:38:53 +0700

    commit web phiim

--- Web/ReactJS/11.Project_Tutorial/Project/src/components/Layout/common/Header/index.js
+++ Web/ReactJS/11.Project_Tutorial/Project/src/components/Layout/common/Header/index.js
@@ -1,10 +1,18 @@
 import './Header.scss'
-
+import {Button, Typography, Image} from 'antd'
 function Header() {
     return (  
         <div className="header">
             <div className="header__inner">
-                <h2>Header</h2>
+           
+                    <Button type="primary" style={{ background: "rgb(28, 28, 30)", border: "none", fontSize: "19px"}}>TV Show</Button>;
+                    <Button type="primary" style={{ background: "rgb(28, 28, 30)", border: "none", fontSize: "19px"}}>Movie</Button>;
+                    <Typography style={{color: "#fff",  fontSize: "19px"}}>Anonymous</Typography>
+                    <Image
+                        width={50}
+                        src="https://moonlight-films.vercel.app/defaultAvatar.jpg"
+                    />
+                    <img style={{width: "50px"}} src="./src/assets/img/header/defaultAvatar.jpg"/>
             </div>
         </div>
     );
